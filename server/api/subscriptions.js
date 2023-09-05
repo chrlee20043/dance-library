f5d2716f9272deaa8f5f50b5ec1a6e6ac294f842
Christina Lee <chrlee93@gmail.com>
2023-09-04 23:04:12 -0700

    writing routes for subscriptions table

--- server/api/subscriptions.js
+++ server/api/subscriptions.js
@@ -1,4 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-const {createSubscription } =
+const {
+  createSubscription,
+  getAllSubscriptions,
+  getSubByUserId,
+} = require("../db/helpers/subscriptions");
+
+const subscriptions = require("../db/seedData");
+
+// GET - api/subscriptions - get all subscriptions
+
+router.get("/", async (req, res, next) => {
+  try {
+    const subscriptions = await getAllSubscriptions();
+    res.send(subscriptions);
+  } catch (error) {
+    console.log("error", error);
+    next(error);
+  }
+});
+
+// GET - /api/:userId/subscription - get subscription by USER id
+
+router.get("/:userId/subscription", async (req, res, next) => {
+  try {
+    const user = await getSubByUserId(req.params.userId);
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- server/db/helpers/subscriptions.js
+++ server/db/helpers/subscriptions.js
@@ -30,4 +30,59 @@ const createSubscription = async ({
   }
 };
 
-module.exports = { createSubscription };
+// GET - /api/subscriptions - get all subscriptions
+
+const getAllSubscriptions = async () => {
+  try {
+    const { rows } = await client.query(`
+            SELECT *
+            FROM subscriptions;
+        `);
+    // console.log(rows);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// GET - /api/subscriptions/:userId - get user by id
+
+const getSubByUserId = async (userId) => {
+  try {
+    const { rows: subscriptions } = await client.query(
+      `
+      SELECT subscriptions.*, users.user_id, users.subscriptionStatus
+      FROM subscriptions
+      JOIN users ON users.user_id = subscriptions.user_id
+      WHERE user_id = ${userId};
+    `[userId]
+    );
+    return users;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// GET - get user by username
+
+async function getUserByUsername(username) {
+  // first get the user
+  try {
+    const { rows } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username = $1;
+    `,
+      [username]
+    );
+    if (!rows || !rows.length) return null;
+    const [user] = rows;
+    delete user.password;
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = { createSubscription, getAllSubscriptions, getSubByUserId };
