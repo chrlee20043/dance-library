917822ebc69af86a689e6a657b3779cc26d51a56
Christina Lee <chrlee93@gmail.com>
2023-10-16 20:02:22 -0700

    updating db string

--- server/db/client.js
+++ server/db/client.js
@@ -7,7 +7,7 @@ const dancelibrary = "dancelibrary";
 
 // const client = new Client(`postgres://localhost:5433/${dancelibrary}`);
 const client = new client(
-  `postgres://dancelibrary_user:IDld0Vrsbb7IsOIfCnn71YzJ7uP2bRjE@dpg-ckmvahv83ejs738j3v1g-a/dancelibrary`
+  `postgres://dancelibrary_user:IDld0Vrsbb7IsOIfCnn71YzJ7uP2bRjE@dpg-ckmvahv83ejs738j3v1g-a/${dancelibrary}`
 );
 
 // Export for use in other files
