97b8d9e8c7b91cf85e2d2a15617e60a7ad8941e9
Christina Lee <chrlee93@gmail.com>
2023-10-16 19:52:39 -0700

    db internal url update

--- server/db/client.js
+++ server/db/client.js
@@ -5,7 +5,10 @@ const { Client } = require("pg");
 
 const dancelibrary = "dancelibrary";
 
-const client = new Client(`postgres://localhost:5433/${dancelibrary}`);
+// const client = new Client(`postgres://localhost:5433/${dancelibrary}`);
+const client = new client(
+  `postgres://dancelibrary_user:IDld0Vrsbb7IsOIfCnn71YzJ7uP2bRjE@dpg-ckmvahv83ejs738j3v1g-a/dancelibrary`
+);
 
 // Export for use in other files
 
