8c8778e6b3090676a6507420662657dc3314ea42
Christina Lee <chrlee93@gmail.com>
2023-10-24 16:54:46 -0700

    organizing component structure, instructors route

---
+++ client/src/components/Instructors/AllInstructors.jsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Grid, TextField, Typography, Select, MenuItem } from "@mui/material";
+import { fetchAllInstructors } from "../../helpers/fetching";
+import AddNewInstructor from "./AddNewInstructor";
+import InstructorListName from "./InstructorListName";
+import { VideosContext } from "../../context/VideosContext";
+
+export default function AllInstructors({ token, userId }) {
+  const { videos, setVideos } = useContext(VideosContext);
+  const [instructors, setInstructors] = useState([]);
+  const [searchParam, setSearchParam] = useState("");
+  const [selectedStyle, setSelectedStyle] = useState("");
+
+  const [error, setError] = useState("");
+
+  const renderInstructors = async () => {
+    try {
+      const instructorArray = await fetchAllInstructors();
+      setInstructors(instructorArray);
+      console.log(instructorArray);
+    } catch (error) {
+      setError("Failed to fetch instructors. Please try again later.");
+    }
+  };
+
+  useEffect(() => {
+    renderInstructors();
+  }, []);
+
+  const textFieldSX = {
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: "rgb(255, 123, 0)",
+    },
+    "& .MuiFormLabel-root.Mui-focused": {
+      color: "rgb(255, 123, 0)",
+    },
+    "& .MuiOutlinedInput-root.Mui-focused": {
+      color: "rgb(255, 123, 0)",
+    },
+  };
+
+  const gridContainerSX = {
+    alignItems: "flex-start",
+  };
+
+  const uniqueStyles = [...new Set(videos.map((video) => video.style))];
+
+  const instructorsToDisplay = instructors.filter((instructor) => {
+    const instructorName = instructor.name.toLowerCase();
+    const style = instructor.style.toLowerCase();
+
+    const typeMatch = selectedStyle !== ""; // Check if a style is selected
+
+    const searchMatch =
+      instructorName.includes(searchParam) || style.includes(searchParam);
+
+    return (
+      typeMatch &&
+      (searchParam === "" || searchMatch) &&
+      (selectedStyle === "" || style === selectedStyle)
+    );
+  });
+
+  const onStyleChange = (e) => {
+    const style = e.target.value;
+    setSelectedStyle(style);
+  };
+
+  return (
+    <Grid container spacing={3} sx={gridContainerSX}>
+      <Grid item xs={12}>
+        <Typography variant="h4">Instructors</Typography>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <TextField
+          fullWidth
+          label="Search"
+          variant="outlined"
+          onChange={(e) => setSearchParam(e.target.value.toLowerCase())}
+          sx={textFieldSX}
+        />
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Select
+          value={selectedStyle}
+          onChange={onStyleChange}
+          displayEmpty
+          fullWidth
+          variant="outlined"
+          sx={textFieldSX}
+        >
+          <MenuItem value="">Search by Style</MenuItem>
+          {uniqueStyles.map((style) => (
+            <MenuItem key={style} value={style}>
+              {style}
+            </MenuItem>
+          ))}
+        </Select>
+      </Grid>
+
+      {token && (
+        <Grid
+          item
+          xs={12}
+          md={4}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          <AddNewInstructor token={token} userId={userId} />
+        </Grid>
+      )}
+      <Grid item xs={12}>
+        {error && <p>{error}</p>}
+        {token ? (
+          <Grid container spacing={3}>
+            {instructorsToDisplay.map((instructor) => (
+              <Grid item key={instructor.instructor_id} xs={12} md={4}>
+                <InstructorListName instructor={instructor} />
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <p>No instructors available</p>
+        )}
+      </Grid>
+    </Grid>
+  );
+}
---
+++ client/src/components/Navigation/Navbar.jsx
@@ -0,0 +1,269 @@
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../Redux/authSlice";
+import { resetFavorites } from "../../Redux/favoriteSlice";
+
+// MUI IMPORTS
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const drawerWidth = 240;
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const onLogout = () => {
+    dispatch(logOut());
+    dispatch(resetFavorites());
+    navigate("/login");
+    console.log("logout token:", props.token);
+  };
+
+  const drawer = (
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{
+        background: "rgb(219, 206, 219)",
+        textAlign: "center",
+        height: "100%",
+      }}
+    >
+      <Typography
+        variant="h6"
+        sx={{ my: 2, color: "white", fontWeight: "bold", fontSize: "22px" }}
+      >
+        MY DANCE LIBRARY
+      </Typography>
+      <Divider />
+      <List
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ListItemButton
+          sx={{ color: "white", fontWeight: "bold", fontSize: "20px" }}
+          color="inherit"
+          component={Link}
+          to="/"
+        >
+          HOME
+        </ListItemButton>
+        {props.token && (
+          <ListItemButton
+            sx={{ color: "white", fontWeight: "bold", fontSize: "20px" }}
+            color="inherit"
+            fontWeight="bold"
+            component={Link}
+            to="/myprofile"
+          >
+            PROFILE
+          </ListItemButton>
+        )}
+        <ListItemButton
+          sx={{ color: "white", fontWeight: "bold", fontSize: "20px" }}
+          color="inherit"
+          fontWeight="bold"
+          component={Link}
+          to="/allvideos"
+        >
+          CLASSES
+        </ListItemButton>
+        <ListItemButton
+          sx={{ color: "white", fontWeight: "bold", fontSize: "20px" }}
+          color="inherit"
+          fontWeight="bold"
+          component={Link}
+          to="/instructors"
+        >
+          INSTRUCTORS
+        </ListItemButton>
+        {props.token ? (
+          <ListItemButton
+            sx={{ color: "white", fontWeight: "bold", fontSize: "20px" }}
+            color="inherit"
+            fontWeight="bold"
+            onClick={onLogout}
+          >
+            LOG OUT
+          </ListItemButton>
+        ) : (
+          <ListItemButton
+            sx={{ color: "white", fontWeight: "bold", fontSize: "20px" }}
+            color="inherit"
+            fontWeight="bold"
+            component={Link}
+            to="/login"
+          >
+            LOGIN
+          </ListItemButton>
+        )}
+      </List>
+    </Box>
+  );
+
+  const listItemButtonSX = {
+    fontSize: "20px",
+    fontWeight: "bold",
+    borderBottom: "2px solid transparent",
+    transition: "border-bottom-color 0.3s ease",
+    "&:hover": {
+      borderBottom: "2px solid rgb(255, 123, 0)",
+    },
+  };
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar component="nav" sx={{ background: "rgb(219, 206, 219)" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", sm: "block" },
+                fontWeight: "bold",
+              }}
+            >
+              MY DANCE LIBRARY
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <List
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "right",
+                }}
+              >
+                <ListItemButton
+                  sx={listItemButtonSX}
+                  color="inherit"
+                  component={Link}
+                  to="/"
+                >
+                  HOME
+                </ListItemButton>
+                {props.token && (
+                  <ListItemButton
+                    sx={listItemButtonSX}
+                    color="inherit"
+                    component={Link}
+                    to="/myprofile"
+                  >
+                    PROFILE
+                  </ListItemButton>
+                )}
+                <ListItemButton
+                  sx={listItemButtonSX}
+                  color="inherit"
+                  component={Link}
+                  to="/allvideos"
+                >
+                  CLASSES
+                </ListItemButton>
+                <ListItemButton
+                  sx={listItemButtonSX}
+                  color="inherit"
+                  component={Link}
+                  to="/instructors"
+                >
+                  INSTRUCTORS
+                </ListItemButton>
+
+                {props.token ? (
+                  <ListItemButton
+                    sx={listItemButtonSX}
+                    color="inherit"
+                    onClick={onLogout}
+                  >
+                    LOG OUT
+                  </ListItemButton>
+                ) : (
+                  <ListItemButton
+                    sx={listItemButtonSX}
+                    color="inherit"
+                    component={Link}
+                    to="/login"
+                  >
+                    LOGIN
+                  </ListItemButton>
+                )}
+              </List>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <nav>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true,
+            }}
+            sx={{
+              background: "rgb(219, 206, 219)",
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </nav>
+        <Box component="main" sx={{ p: 3 }}>
+          <Toolbar />
+          <Typography></Typography>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+DrawerAppBar.propTypes = {
+  window: PropTypes.func,
+  token: PropTypes.string,
+};
+
+export default DrawerAppBar;
